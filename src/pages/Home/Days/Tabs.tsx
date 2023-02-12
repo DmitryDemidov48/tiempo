64e01cf7d8ddddcc45674999949647dd0a2b2083
RadKiSSGaS <108529559+DmitryDemidov48@users.noreply.github.com>
2023-02-12 15:31:38 -0800

    first commit

---
+++ src/pages/Home/Days/Tabs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Days.module.scss'
+export const Tads = () => {
+    return (
+        <div className={s.tabs}>
+            <div className={s.tabs__wrapper}></div>
+        </div>
+    );
+};
+
